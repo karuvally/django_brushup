e76a52275d5984fd8ccae9468f1f0dcf97eeeecf
Aswin Karuvally <aswinbabuk@gmail.com>
2021-08-30 18:03:42 +0530

    display category, post names in django-admin

--- blog/models.py
+++ blog/models.py
@@ -3,12 +3,20 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=20)
 
+    # Display category names in Django Admin
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField("Category", related_name="posts")
+
+    # Display Post name in Django Admin
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
     author = models.CharField(max_length=60)
