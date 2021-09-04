2e4fdfb4f147698aac0908f9ea034b7b035f8c05
Aswin Karuvally <aswinbabuk@gmail.com>
2021-09-04 16:53:54 +0530

    removed login app urls

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -20,5 +20,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("projects/", include('projects.urls')),
     path("blog/", include("blog.urls")),
-    path("login/", include("login.urls"))
 ]
