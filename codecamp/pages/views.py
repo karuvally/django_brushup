00975fc9e4ec35505ff1aab41bcd2220a12d9889
Aswin Karuvally <aswinbabuk@gmail.com>
2021-09-10 21:59:04 +0530

    added render for homepage

--- codecamp/pages/views.py
+++ codecamp/pages/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Welcome to Home Page!<h1>")
+    # return HttpResponse("<h1>Welcome to Home Page!<h1>")
+    render(request, "home.html", {})
 
 def contact_view(request, *args, **kwargs):
     return HttpResponse("<h1>All contacts can be found here</h1>")
