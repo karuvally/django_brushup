fa50b7c10fe4e6779789fe535aa12f850a3fddc4
Aswin Karuvally <aswinbabuk@gmail.com>
2021-09-10 22:10:47 +0530

    added homepage HTML

--- codecamp/pages/views.py
+++ codecamp/pages/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home_view(request, *args, **kwargs):
     # return HttpResponse("<h1>Welcome to Home Page!<h1>")
-    render(request, "home.html", {})
+    return render(request, "home.html", {})
 
 def contact_view(request, *args, **kwargs):
     return HttpResponse("<h1>All contacts can be found here</h1>")
