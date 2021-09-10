bb6b2a4d02ba0a25b2bccf6e6281d42375d8e18a
Aswin Karuvally <aswinbabuk@gmail.com>
2021-09-10 18:39:44 +0530

    added views for pages app

--- codecamp/pages/views.py
+++ codecamp/pages/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home_view(*args, **kwargs):
     return HttpResponse("<h1>Welcome to Home Page!<h1>")
+
+def contact_view(*args, **kwargs):
+    return HttpResponse("<h1>All contacts can be found here</h1>")
