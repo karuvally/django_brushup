d5e65ce0aa254f82766c48ad13ca823438cda329
Aswin Karuvally <aswinbabuk@gmail.com>
2021-09-10 21:53:05 +0530

    updated views

--- codecamp/pages/views.py
+++ codecamp/pages/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home_view(*args, **kwargs):
+def home_view(request, *args, **kwargs):
     return HttpResponse("<h1>Welcome to Home Page!<h1>")
 
-def contact_view(*args, **kwargs):
+def contact_view(request, *args, **kwargs):
     return HttpResponse("<h1>All contacts can be found here</h1>")
