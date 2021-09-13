ced6620593ecd7709fa654a961bc689475dfd06b
Aswin Karuvally <aswinbabuk@gmail.com>
2021-09-13 11:43:36 +0530

    added product_detail_view

--- codecamp/products/views.py
+++ codecamp/products/views.py
@@ -1,1 +1,15 @@
 from django.shortcuts import render
+from .models import Product
+
+def product_detail_view(request):
+    products = Product.objects.all()
+    context = {"products": []}
+    for product in products:
+        context["products"].append({
+            "title": product.title,
+            "description": product.description,
+            "price": product.price,
+            "summary": product.summary,
+            "featured": product.featured,
+        })
+    return render(request, "product/detail.html", context)
