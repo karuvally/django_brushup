377b2f452c8e555db4f29b7d96447fda846a9331
Aswin Karuvally <aswinbabuk@gmail.com>
2021-09-13 11:51:56 +0530

    updated product_list_view

--- codecamp/products/views.py
+++ codecamp/products/views.py
@@ -7,9 +7,6 @@ def product_list_view(request):
     for product in products:
         context["products"].append({
             "title": product.title,
-            "description": product.description,
             "price": product.price,
-            "summary": product.summary,
-            "featured": product.featured,
         })
     return render(request, "product/list.html", context)
