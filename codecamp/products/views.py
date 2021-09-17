db4ae23ebf61ae4267363cbe6314ae9fe9d502ba
Aswin Karuvally <aswinbabuk@gmail.com>
2021-09-17 17:16:06 +0530

    updated raw form behaviour

--- codecamp/products/views.py
+++ codecamp/products/views.py
@@ -17,7 +17,9 @@ def product_raw_form_view(request):
     raw_form = ProductRawForm()
     if request.method == "POST":
         raw_form = ProductRawForm(request.POST)
-    print("Hello")
+        if raw_form.is_valid():
+            print(raw_form.cleaned_data)
+        raw_form = ProductRawForm()
     context = {
         "raw_form": raw_form,
     }
