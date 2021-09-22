865c088ced00c99b8ac3f4c66ed5bb60dcc9107b
Aswin Karuvally <aswinbabuk@gmail.com>
2021-09-22 10:45:53 +0530

    implemented products.urls in projecT

--- codecamp/codecamp/urls.py
+++ codecamp/codecamp/urls.py
@@ -14,23 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from pages.views import home_view, contact_view
-from products.views import product_list_view, product_detail_view
-from products.views import product_model_form_view, product_html_form_view
-from products.views import product_raw_form_view, product_model_form_change_view
-from products.views import product_delete_view
+
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('contact/', contact_view, name='contact'),
-    path('product_list/', product_list_view, name='product_list'),
-    path('product/<int:id_lookup>/', product_detail_view, name='product_detail'),
-    path('model_form/', product_model_form_view, name='model_form'),
-    path('model_change_form/', product_model_form_change_view, name='model_change'),
-    path('html_form/', product_html_form_view, name='html_form'),
-    path('raw_form/', product_raw_form_view, name='raw_form'),
-    path('delete_form/', product_delete_view, name='delete_form'),
     path('admin/', admin.site.urls),
+    path("product/", include("products.urls")),
 ]
--- codecamp/products/urls.py
+++ codecamp/products/urls.py
@@ -10,4 +10,12 @@ from products.views import (
     product_delete_view,
 )
 
-
+urlpatterns = [
+    path('product_list/', product_list_view, name='product_list'),
+    path('product/<int:id_lookup>/', product_detail_view, name='product_detail'),
+    path('model_form/', product_model_form_view, name='model_form'),
+    path('model_change_form/', product_model_form_change_view, name='model_change'),
+    path('html_form/', product_html_form_view, name='html_form'),
+    path('raw_form/', product_raw_form_view, name='raw_form'),
+    path('delete_form/', product_delete_view, name='delete_form'),
+]
