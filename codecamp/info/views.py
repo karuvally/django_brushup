e1daef7dea0e99fb46bc2f862e2d469d4891f5d4
Aswin Karuvally <aswinbabuk@gmail.com>
2021-09-24 19:32:02 +0530

    info_home_view now calls update_sensor_data()

--- codecamp/info/views.py
+++ codecamp/info/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Sensors
 
 # Create your views here.
 def info_home_view(request):
+    sensors = Sensors()
+    sensors.update_sensor_data()
     return render(request, "info_home.html", {})
